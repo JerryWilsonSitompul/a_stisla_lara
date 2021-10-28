2dab8e1f7b928d690afdc343aeee2b4e9c77600c
Jerry Wilson Sitompul <jerrywilsonsitompul@gmail.com>
2021-10-28 13:15:51 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+
+// Route::get('/', function () {
+//     return view('');
+// });
+
+// Route::name('admin.')->prefix('admin')->middleware('auth')->group(function() {
+//     Route::get('dashboard', 'DashboardController')->name('dashboard');
+
+//     Route::get('users/roles', 'UserController@roles')->name('users.roles');
+//     Route::resource('users', 'UserController', [
+//         'names' => [
+//             'index' => 'users'
+//         ]
+//     ]);
+// });
+
+Route::get('/', 'HomeController@index');
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/profile', 'UserController@show')->name('profile');
+
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
